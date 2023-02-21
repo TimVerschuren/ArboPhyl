9f3b74bab3b001408f90b3ca76d4364762110703
Tim Verschuren <timtwig.verschuren@gmail.com>
2023-02-21 10:07:40 +0100

    Added python and bash scripts for ArboPhyl

---
+++ Arbophyl.py
@@ -0,0 +1,162 @@
+# ArboPhyl version 1.0
+# 20-02-2023
+# Author: Tim Verschuren
+
+import subprocess
+import os
+import collections
+
+def title():
+    print(f"+-------------------------------------------------+")
+    print(f"|\033[92m    ___       _\033[00m           ______ _           _   |")
+    print(f"|\033[92m   / _ \     | |\033[00m          | ___ \ |         | |  |")
+    print(f"|\033[92m  / /_\ \_ __| |__   ___ \033[00m | |_/ / |__  _   _| |  |")
+    print(f"|\033[92m  |  _  | '__| '_ \ / _ \\\033[00m |  __/| '_ \| | | | |  |")
+    print(f"|\033[92m  | | | | |  | |_) | (_) |\033[00m| |   | | | | |_| | |  |")
+    print(f"|\033[92m  \_| |_/_|  |_.__/ \___/\033[00m \_|   |_| |_|\__, |_|  |")
+    print(f"|                     ----------+        __/ |    |")
+    print(f"|     --------------------+     |-------|___/     |")
+    print(f"|                         |-----+                 |")
+    print(f"|           --------------+                       |")
+    print(f"+-------------------------------------------------+")
+    print("Welcome to \033[92mArbo\033[00mPhyl!\n| 0: Full pipeline\n| 1: BUSCO\n| 2: FilterBUSCOs\n| 3: MAFFT\n| 4: TrimAl\n| 5: IQTREE Model Prediction\n| 6: Partition file creation\n| 7: IQTREE")
+
+
+def analysis_selection():
+    input_list = input("Select number(s) of required analyses seperated by commas (1,2,3): ")
+    analyses = input_list.split(",")
+    # Make sure that a program is not run multiple times
+    if len(analyses) >> 1 & analyses.__contains__("0"):
+        analyses = ["0"]
+    # Make sure that no letters are used as an input
+    for item in analyses:
+        try:
+            int(item)
+        except:
+            print("Please only input integers!")
+            analysis_selection()
+
+    lineage = input("Please input the lineage used during the BUSCO analysis: ").lower()
+    return(analyses, lineage)
+
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '█' * int(percent) + '-' * (100 - int(percent))
+    print(f"\r Progress: |{bar}| {percent:.0f}%", end = "\r")
+
+
+def find_overlap(lineage):
+
+    total_folders = 0
+    busco_dict = {}
+    folders = os.listdir()
+
+    for file in folders:
+        if file.__contains__("."):
+            pass
+        elif file.__contains__("busco_downloads"):
+            pass
+        elif file.__contains__("FilterBUSCOs_output"):
+            pass
+        else:
+            total_folders += 1
+            busco_list = []
+            # Creates the filepath based on submitted database
+            path = f"{os.path.dirname(os.path.abspath(file))}/{file}/run_{lineage}_odb10/busco_sequences/single_copy_busco_sequences"
+            for file_name in os.listdir(path):
+                # Place all fasta files into a list
+                if file_name.endswith('.fna'):
+                    busco_list.append(file_name)
+                    if file_name not in busco_dict.keys():
+                        busco_dict[file_name] = 1
+                    else:
+                        busco_dict[file_name] += 1
+                else:
+                    pass
+    return [busco_dict, total_folders, lineage]
+
+        
+def busco_MSA(inputs):
+
+    folders= os.listdir()
+    i = 0
+
+    for key, value in inputs[0].items():
+        if value == inputs[1]:
+            output_file_name = f"FilterBUSCOs_output/{key.replace('.fna', '_MS.fna')}"
+            os.makedirs(os.path.dirname(output_file_name), exist_ok=True)
+            with open(output_file_name, "w") as MSA_fasta:
+                for file in folders:
+                    if file.__contains__("."):
+                        pass
+                    elif file.__contains__("busco_downloads"):
+                        pass
+                    elif file.__contains__("FilterBUSCOs_output"):
+                        pass
+                    else:
+                        path = f"{os.path.dirname(os.path.abspath(file))}/{file}/run_{inputs[2]}_odb10/busco_sequences/single_copy_busco_sequences"
+                        for file_name in os.listdir(path):
+                            if file_name == key:
+                                with open(f"{path}/{file_name}", "r") as fasta_read:
+                                    fasta_seq = fasta_read.readlines()
+                                    for line in fasta_seq:
+                                        if line.__contains__(">"):
+                                            MSA_fasta.write(line.replace(line, f">{file}\n"))
+                                        else:
+                                            MSA_fasta.write(line)
+            i += 1
+            progress_bar(i, collections.Counter(inputs[0].values())[inputs[1]])
+    print("\n")
+
+
+def create_partition_file():
+
+    model_dict = {}
+    path = f"{os.getcwd()}/FilterBUSCOs_output/MAFFT_output/Trimmed_MSAs/Models"
+    print(path)
+    with open("Partition.nex", "w") as partition_file:
+        partition_file.write("#nexus\nbegin sets;\n")
+        for dir in os.listdir(path):
+            if dir.__contains__(".") or dir.__contains__("_"):
+                pass
+            else:
+                partition_file.write(f"\tcharset {dir} = FilterBUSCOs_output/MAFFT_output/Trimmed_MSAs/{dir}_trimmed.fna: *;\n")
+                with open(f"{path}/{dir}/{dir}_trimmed.fna.iqtree", "r") as models:
+                    for line in models:
+                        if "BIC:" in line:
+                            model_dict[dir] = (line.split(": ")[1]).replace("\n", "")
+        partition_file.write("\tcharpartition mine = ")
+        for key, value in model_dict.items():
+            partition_file.write(f"{value}:{key}, ")
+        partition_file.write(";\nend;")
+
+
+def analysis_exe(inputs):
+    # Start BUSCO
+    if inputs[0].__contains__("0") or inputs[0].__contains__("1"):
+        print(subprocess.run(["bash", "-i", "Arbophyl.sh", "busco", inputs[1]]))
+    # Start FilterBUSCOs
+    if inputs[0].__contains__("0") or inputs[0].__contains__("2"):
+        busco_MSA((find_overlap(inputs[1])))
+    # Start MAFFT
+    if inputs[0].__contains__("0") or inputs[0].__contains__("3"):
+        print(subprocess.run(["bash", "-i", "Arbophyl.sh", "mafft"]))
+    # Start TrimAl
+    if inputs[0].__contains__("0") or inputs[0].__contains__("4"):
+        print(subprocess.run(["bash", "-i", "Arbophyl.sh", "trimal"]))
+    # Start IQTREE Models
+    if inputs[0].__contains__("0") or inputs[0].__contains__("5"):
+        print(subprocess.run(["bash", "-i", "Arbophyl.sh", "iqtree_models"]))
+    # Start Partition file creation
+    if inputs[0].__contains__("0") or inputs[0].__contains__("6"):
+        create_partition_file()
+    # Start IQTREE
+    if inputs[0].__contains__("0") or inputs[0].__contains__("7"):
+        print(subprocess.run(["bash", "-i", "Arbophyl.sh", "iqtree"]))
+
+
+title()
+analysis_exe(analysis_selection())
+
+print("Finished all processes, thank you for using \033[92mArbo\033[00mPhyl!")
---
+++ Arbophyl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [[ "$1" == "busco" ]]; then
+    conda activate busco
+    for file in *fasta
+    do
+        busco -i $file -o ${file/_*/""}/ -l $2 -m genome -c 6
+    done
+fi
+
+if [[ "$1" == "mafft" ]]; then
+    cd FilterBUSCOs_output/
+    conda activate mafft
+    mkdir MAFFT_output
+    for file in *fna
+    do
+        mafft --auto --inputorder --thread 20 "$file" > "MAFFT_output/${file/MS/"MSA"}"
+    done
+fi
+
+if [[ "$1" == "trimal" ]]; then
+    cd FilterBUSCOs_output/MAFFT_output/
+    conda activate trimal
+    mkdir Trimmed_MSAs
+    mkdir Trimmed_MSAs/Models
+    for file in *fna
+    do
+        mkdir Trimmed_MSAs/Models/${file/_MSA.fna/""}
+        trimal -in $file -out Trimmed_MSAs/${file/MSA/"trimmed"} -strict
+        cp Trimmed_MSAs/${file/MSA/"trimmed"} Trimmed_MSAs/Models/${file/_MSA.fna/""}
+    done
+fi
+
+if [[ "$1" == "iqtree_models" ]]; then
+    cd FilterBUSCOs_output/MAFFT_output/Trimmed_MSAs/Models
+    conda activate iqtree
+    for dir in *
+    do
+    cd $dir
+    iqtree -s *.fna -m MF -nt 20
+    cd ..
+    done
+fi
+
+if [[ "$1" == "iqtree" ]]; then
+    conda activate iqtree
+    iqtree -s FilterBUSCOs_output/MAFFT_output/Trimmed_MSAs/ -p -nt 20 Partition.nex -bb 1000 -alrt 1000
+fi
